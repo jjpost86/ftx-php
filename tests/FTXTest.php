aa13b1bc9d66fbec4b3bf760d7f38e4057894f6c
Pipelines <jjpost@ucdavis.edu>
2021-02-15 08:30:26 -0800

    Init

--- tests/FTXTest.php
+++ tests/FTXTest.php
@@ -16,66 +16,31 @@ use FTX\Api\Options;
 use FTX\Api\Orders;
 use FTX\Api\Subaccounts;
 use FTX\Api\Wallet;
-use FTX\Client\HttpClient;
-use FTX\FTX;
+use FTX\Client\HttpClientUS;
+use FTX\FTXUS;
 
-class FTXTest extends FTXTestCase
+class FTXUSTest extends FTXTestCase
 {
     public function testFTXAcceptsHttpClient()
     {
-        $http = new HttpClient(
+        $http = new HttpClientUS(
             Psr18ClientDiscovery::find(),
             Psr17FactoryDiscovery::findRequestFactory(),
             Psr17FactoryDiscovery::findUrlFactory(),
             Psr17FactoryDiscovery::findStreamFactory(),
-            'https://ftx.com/api'
+            'https://ftx.us/api'
         );
         
-        $ftx = new FTX($http);
+        $ftx = new FTXUS($http);
         
         $this->assertEquals($ftx->getClient(), $http);
     }
     
     public function testFTXAutomaticallyCreatesHttpClient()
     {
-        $ftx = FTX::create();
+        $ftx = FTXUS::create();
         
-        $this->assertInstanceOf(HttpClient::class, $ftx->getClient());
+        $this->assertInstanceOf(HttpClientUS::class, $ftx->getClient());
     }
-    
-    public function testFTXAcceptsCredentials()
-    {
-        $ftx = FTX::create('foo', 'bar');
-        
-        $this->assertEquals('foo', $ftx->getClient()->api_key);
-        $this->assertEquals('bar', $ftx->getClient()->api_secret);
-    }
-    
-    public function testIsPossibleToSpecifySubaccount()
-    {
-        $ftx = FTX::create('foo', 'bar');
-        $ftx->onSubaccount('foo');
 
-        $this->assertEquals('foo', $ftx->getClient()->api_key);
-        $this->assertEquals('bar', $ftx->getClient()->api_secret);
-        $this->assertEquals('foo', $ftx->getClient()->subaccount);
-    }
-    
-    public function testAPIEndpoints()
-    {
-        $ftx = FTX::create();
-        
-        $this->assertInstanceOf(Subaccounts::class, $ftx->subaccounts());
-        $this->assertInstanceOf(Markets::class, $ftx->markets());
-        $this->assertInstanceOf(Futures::class, $ftx->futures());
-        $this->assertInstanceOf(Account::class, $ftx->account());
-        $this->assertInstanceOf(Wallet::class, $ftx->wallet());
-        $this->assertInstanceOf(Orders::class, $ftx->orders());
-        $this->assertInstanceOf(ConditionalOrders::class, $ftx->conditionalOrders());
-        $this->assertInstanceOf(Fills::class, $ftx->fills());
-        $this->assertInstanceOf(FundingPayments::class, $ftx->fundingPayments());
-        $this->assertInstanceOf(LeveragedTokens::class, $ftx->leveragedTokens());
-        $this->assertInstanceOf(Options::class, $ftx->options());
-        $this->assertInstanceOf(SpotMargin::class, $ftx->spotMargin());
-    }
 }
